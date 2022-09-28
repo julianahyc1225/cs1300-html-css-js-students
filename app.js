3abd0b491f9520623ce3e0b4685b05766e38fdb3
julianahyc1225 <julianahyc1225@hotmail.com>
2022-09-27 21:02:24 -0400

    handin

--- app.js
+++ app.js
@@ -26,14 +26,26 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  //console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
+  const filteredArray = fruitsArray.filter((item)=>{
+    return item.nutritions.sugar > 15;
+  })
+
+  const nameArray = filteredArray.map((item)=>{
+    return item.name;
+  })
+  console.log(nameArray);
+
   // TODO: Create a new HTML element to display your data
+  const sweetFruits = document.createElement('div');
+  sweetFruits.innerHTML = nameArray;
 
   // TODO: Append your new element to the page
+  gallery.append(sweetFruits);
 
 }
 
